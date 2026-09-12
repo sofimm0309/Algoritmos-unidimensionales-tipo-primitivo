f86c0ec142f1f5b2cc192d353ded024a33c84145
sofimm0309 <sofia.marinmorales@ucr.ac.r>
2026-09-12 10:49:42 -0600

    commitprueba

---
+++ src/main/java/com/mycompany/algoritmos_unidimensionales/Algoritmos_unidimensionales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmos_unidimensionales;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Algoritmos_unidimensionales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
