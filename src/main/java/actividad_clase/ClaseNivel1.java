78a466c703a4092ec26c63b8db40396a8571bc46
sofimm0309 <sofia.marinmorales@ucr.ac.r>
2026-09-12 15:07:19 -0600

    Clase 1 y Menú actualizados

--- src/main/java/actividad_clase/ClaseNivel1.java
+++ src/main/java/actividad_clase/ClaseNivel1.java
@@ -4,10 +4,34 @@
  */
 package actividad_clase;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Gerardo
  */
 public class ClaseNivel1 {
-    
+
+    //------------------Algoritmos Sofía
+    //-----Algoritmo 1
+    public static void algoritmo1() {
+        //variables
+        double[] numeros = {1.2, 2.3, 4.5, 6.7};
+        //salidas
+        JOptionPane.showMessageDialog(null, "Algoritmo 1\n"
+                + "\nLongitud del arreglo: " + numeros.length
+                + "\nÚltimo elemento: " + numeros[numeros.length - 1]);
+
+    }
+
+    public static void algoritmo2() {
+        boolean[] valores = {true, false, true, true, false, false};
+        for (int i = 0; i < valores.length; i++) {
+
+            JOptionPane.showMessageDialog(null, "Algoritmo 2\n"
+                                                + "Posición " + i + ": " + valores[i]);
+
+        }
+
+    }
 }
--- src/main/java/actividad_clase/Menu.java
+++ src/main/java/actividad_clase/Menu.java
@@ -19,20 +19,25 @@ public class Menu {
 
 //do-while para correr el menú
         do {
-        opcion = Integer.parseInt(JOptionPane.showInputDialog(
-                "=== MENÚ PRINCIPAL ===\n"
-                + "1. Nivel 1 - Fundamentos\n"
-                + "2. Nivel 2 - Recorridos y acumulación\n"
-                + "3. Nivel 3 - Búsqueda, extremos y condiciones\n"
-                + "4. Nivel 4 - Transformación de arreglos\n"
-                + "5. Nivel 5 - Integración y retos\n"
-                + "6. Salir\n\n"
-                + "Digite una opción:"
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(
+                    "=== MENÚ PRINCIPAL ===\n"
+                    + "1. Nivel 1 - Fundamentos\n"
+                    + "2. Nivel 2 - Recorridos y acumulación\n"
+                    + "3. Nivel 3 - Búsqueda, extremos y condiciones\n"
+                    + "4. Nivel 4 - Transformación de arreglos\n"
+                    + "5. Nivel 5 - Integración y retos\n"
+                    + "6. Salir\n\n"
+                    + "Digite una opción:"
             ));
 
             //Switch para usar opciones
             switch (opcion) {
                 case 1:
+                    JOptionPane.showMessageDialog(null, "Algoritmo 1");
+                    ClaseNivel1.algoritmo1();
+                 
+                    JOptionPane.showMessageDialog(null, "Algoritmo 2");
+                    ClaseNivel1.algoritmo2();
                     break;
                 case 2:
                     break;
@@ -43,10 +48,10 @@ public class Menu {
                 case 5:
                     break;
                 case 6:
-                   JOptionPane.showMessageDialog(null, "Saliendo del menú...");
+                    JOptionPane.showMessageDialog(null, "Saliendo del menú...");
                     System.exit(0);
                 default:
-                   JOptionPane.showMessageDialog(null, "Opción inválida");
+                    JOptionPane.showMessageDialog(null, "Opción inválida");
 
             }
 
