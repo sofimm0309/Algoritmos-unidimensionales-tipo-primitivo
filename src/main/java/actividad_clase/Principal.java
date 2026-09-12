6b1903e19753dfb6db9ac53c8bd82da29e75e740
sofimm0309 <sofia.marinmorales@ucr.ac.r>
2026-09-12 11:53:01 -0600

    Agregar clases Nivel1-5 y Menu, actualizar Principal

--- src/main/java/actividad_clase/Principal.java
+++ src/main/java/actividad_clase/Principal.java
@@ -1,14 +1,16 @@
+package actividad_clase;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.algoritmos_unidimensionales;
+
 
 /**
  *
  * @author Gerardo
  */
-public class Algoritmos_unidimensionales {
+public class Principal {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
